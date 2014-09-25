0d9612c17692d9aff71464ee2c78ac0f79cc5dee
Lars Nielsen <tools@h367.aau1x.klient.njv12.site.aau.dk>
2014-09-25 12:04:27 +0200

    begin moddyfing encoder wraper cons

--- src/kodocpp/encoder_factory_wrapper.hpp
+++ src/kodocpp/encoder_factory_wrapper.hpp
@@ -1,3 +1,4 @@
+
 // Copyright Steinwurf ApS 2011-2013.
 // Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
 // See accompanying file LICENSE.rst or
@@ -20,6 +21,15 @@ namespace kodo
             factory_wrapper<KodoStack>(max_symbols, max_symbol_size)
         { }
 
+        //example new constructor
+        //kodo::factory f = kodo::factory(kodo::full_rlnc,
+        //kodo::binary8, 32, 1024, false);
+        void encoder_factory_semi_constructor(uint32_t max_symbols,
+                                              uint32_t max_symbol_size,
+                                              bool trace_enabled)
+        {
+
+        }
         virtual void* build()
         {
             auto encoder = m_factory.build();
