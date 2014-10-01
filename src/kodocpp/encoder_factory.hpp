67887a0dadcd90e5e63ecd3b4aadfa6f04cdd062
Lars Nielsen <tools@Larss-MacBook-Pro.local>
2014-10-01 21:48:42 +0200

    begin adding encoder idea, might need to recode all

---
+++ src/kodocpp/encoder_factory.hpp
@@ -0,0 +1,18 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+class encoder_factory : public factory
+{
+public:
+    encoder(size_t encoder_type) : factory(encoder_type)
+    {
+
+    }
+
+    encoder build()
+    {
+        return encoder(new encoder_wrapper(m_wrapper.build()));
+    }
+}
