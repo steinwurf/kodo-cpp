45016f33792998a5b6d51e1dfd99d7ce94047afd
Lars Nielsen <lnc13.lars@gmail.com>
2014-10-06 13:26:51 +0200

    encoder_wrapper.hpp implemted functions and encoder creater

--- src/kodocpp/encoder.hpp
+++ src/kodocpp/encoder.hpp
@@ -4,60 +4,59 @@
 // http://www.steinwurf.com/licensing
 
 #pragma once
-#include "encoder_interface.hpp"
+#include "encoder_wrapper.hpp"
 
 namespace kodo
 {
-
-    template<class KodoStack>
-    class encoder_wrapper
+    class encoder
     {
     public:
-        encoder_wrapper(KodoStack* stack) : m_encoder(stack)
+        encoder(encoder_interface* wrapper) : m_wrapper(wrapper)
         {
 
         }
 
         virtual uint32_t encode(uint8_t* data)
         {
-            return m_encoder->encode(data);
+            return m_wrapper.encode(data);
         }
 
         virtual void set_symbols(const uint8_t* data, uint32_t size)
         {
-            m_encoder->set_symbols(data, size);
+            m_wrapper.set_symbols(data, size);
         }
 
         virtual void set_symbol(uint32_t index, uint8_t* data, uint32_t size)
         {
-            m_encoder->set_symbol(index, data, size);
+            m_wrapper.set_symbol(index, data, size);
         }
 
         virtual bool has_systematic_encoder() const
         {
-            return kodo::has_systematic_encoder<KodoStack>::value;
+            return m_wrapper.has_systematic_encoder();
         }
 
         virtual bool is_systematic_on() const
         {
-            return kodo::is_systematic_on(m_encoder);
+            return m_wrapper.is_systematic_on();
         }
 
         virtual void set_systematic_on()
         {
-            kodo::set_systematic_on(m_encoder);
+            m_wrapper.set_systematic_on(m_encoder);
         }
         virtual void set_systematic_off()
         {
-            kodo::set_systematic_off(m_encoder);
+            m_wrapper.set_systematic_off();
         }
 
         virtual void read_feedback(uint8_t* feedback)
         {
-            kodo::read_feedback(m_encoder, feedback);
+            m_wrapper.read_feedback(feedback);
         }
 
     private:
-        typename KodoStack::pointer m_encoder;
+        encoder_interface* m_wrapper:
     }
+
 }
