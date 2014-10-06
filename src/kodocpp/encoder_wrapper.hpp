e05ade480317ab46a1e3bcaac08f83c754a73a00
Lars Nielsen <lnc13.lars@gmail.com>
2014-10-06 13:06:23 +0200

    add funs to encoder_wrapper

--- src/kodocpp/encoder_wrapper.hpp
+++ src/kodocpp/encoder_wrapper.hpp
@@ -20,13 +20,47 @@ namespace kodo
 
         virtual uint32_t encode(uint8_t* data)
         {
-            return mStack.encode(data);
+            return mStack->encode(data);
+        }
+
+        virtual void set_symbols(const uint8_t* data, uint32_t size)
+        {
+            mStack->set_symbols(data, size);
+        }
+
+        virtual void set_symbol(uint32_t index, uint8_t* data, uint32_t size)
+        {
+            mStack->set_symbol(index, data, size);
+        }
+
+        //ASK JEPPE
+        virtual bool has_systematic_encoder() const
+        {
+            return mStack->has_systematic_encoder();
+        }
+
+
+        virtual bool is_systematic_on() const
+        {
+            return mStack->is_systematic_on();
+        }
+        //
+
+        virtual void set_systematic_on()
+        {
+            kodo::set_systematic_on();
+        }
+        virtual void set_systematic_off()
+        {
+            kodo::set_systematic_off();
+        }
+
+        virtual void read_feedback(uint8_t* feedback)
+        {
+            kodo::read_feedback(stack, feedback);
         }
 
     private:
         KodoStack* mStack;
     }
-
-
-
 }
