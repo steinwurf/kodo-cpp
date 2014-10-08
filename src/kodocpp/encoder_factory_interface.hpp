866762f29c44e8e0b50ea8fd42f586d64e087cce
Lars Nielsen <tools@Larss-MacBook-Pro.local>
2014-10-08 14:22:14 +0200

    add correct virual implementation

--- src/kodocpp/encoder_factory_interface.hpp
+++ src/kodocpp/encoder_factory_interface.hpp
@@ -11,17 +11,6 @@ namespace kodo
     class encoder_factory_interface : public factory_interface
     {
     public:
-        encoder_wrapper* build()
-        {
-            return NULL;
-        }
-
-        virtual void set_symbols(uint32_t symbols) = 0;
-        virtual void set_symbol_size(uint32_t symbol_size) = 0;
-
-        virtual uint32_t max_symbols() const = 0;
-        virtual uint32_t max_symbol_size() const = 0;
-        virtual uint32_t max_block_size() const = 0;
-        virtual uint32_t max_payload_size() const = 0;
+        virtual encoder_interface* build() = 0;
     };
 }
