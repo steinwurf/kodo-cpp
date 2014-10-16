58abda6506f7a772c9c444f71c5cb4ef96e5b00b
Lars Nielsen <lnc13.lars@gmail.com>
2014-10-16 11:00:27 +0200

    add enums for algorithm and field type

---
+++ src/kodocpp/algorithm.hpp
@@ -0,0 +1,4 @@
+namespace kodo
+{
+    enum algorithm{kodo_full_rlnc, kodo_on_the_fly, kodo_sliding_window}
+}
---
+++ src/kodocpp/field_type.hpp
@@ -0,0 +1,11 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+namespace kodo
+{
+    enum field_type{kodo_binary, kodo_binary8, kodo_binary16}
+}
