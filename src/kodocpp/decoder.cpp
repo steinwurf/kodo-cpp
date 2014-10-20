49da62e163ca5b00e0307cf213b5e3223e5f52a0
Lars Nielsen <lnc13.lars@gmail.com>
2014-10-20 14:33:46 +0200

    split decoder into hpp/cpp and implemented missing methods

--- src/kodocpp/decoder.cpp
+++ src/kodocpp/decoder.cpp
@@ -14,17 +14,51 @@ namespace kodo
 
     void decoder::decode(uint8_t data)
     {
-        return m_wrapper.decode(data);
+        m_wrapper.decode(data);
     }
 
-    bool decoder::is_complete()
+    bool decoder::is_complete() const
     {
         return m_wrapper.is_complete();
     }
 
-    void decoder::copy_symbols(uint8_t data, uint32_t size)
+    void decoder::copy_symbols(uint8_t data, uint32_t size) const
     {
-        return m_wrappercopy_symbols(index, data, size);
+        m_wrapper.copy_symbols(index, data, size);
     }
 
+    void decoder::copy_symbol(uint32_t index, uint8_t* data, uint32_t size) const
+    {
+        m_wrapper.copy_symbol(index, data, size);
+    }
+
+    bool decoder::has_partial_decoding_tracker() const
+    {
+        return m_wrapper.has_partial_decoding_tracker();
+    }
+
+    bool decoder::is_partial_complete() const
+    {
+        return m_wrapper.is_partial_complete();
+    }
+
+    bool decoder::is_symbol_uncoded(uint32_t index) const
+    {
+        return m_wrapper.is_symbol_uncoded(index);
+    }
+
+    uint32_t decoder::symbols_uncoded() const
+    {
+        return m_wrapper.symbols_uncoded();
+    }
+
+    uint32_t decoder::symbols_seen() const
+    {
+        return m_wrapper.symbols_seen();
+    }
+
+    void decoder::write_feedback(uint8_t* feedback)
+    {
+        m_wrapper.write_feedback(feedback);
+    }
 }
--- src/kodocpp/decoder.hpp
+++ src/kodocpp/decoder.hpp
@@ -3,6 +3,8 @@
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
 
+#include "internal/decoder_wrapper.hpp"
+
 namespace kodo
 {
     class decoder
@@ -15,23 +17,23 @@ namespace kodo
 
         ~decoder_interface() {}
 
-        uint32_t recode(uint8_t* data) = 0;
-        void decode(uint8_t* data) = 0;
-        bool is_complete() const = 0;
+        uint32_t recode(uint8_t* data);
+        void decode(uint8_t* data);
+        bool is_complete() const;
 
-        void copy_symbols(uint8_t* data, uint32_t size) const = 0;
+        void copy_symbols(uint8_t* data, uint32_t size) const;
 
         void copy_symbol(uint32_t index, uint8_t* data,
-                                 uint32_t size) const = 0;
+                                 uint32_t size) const;
 
-        bool has_partial_decoding_tracker() const = 0;
+        bool has_partial_decoding_tracker() const;
 
-        bool is_partial_complete() const = 0;
-        bool is_symbol_uncoded(uint32_t) const = 0;
+        bool is_partial_complete() const;
+        bool is_symbol_uncoded(uint32_t index) const;
 
-        uint32_t symbols_uncoded() const = 0;
-        uint32_t symbols_seen() const = 0;
-        void write_feedback(uint8_t* feedback) = 0;
+        uint32_t symbols_uncoded() const;
+        uint32_t symbols_seen() const;
+        void write_feedback(uint8_t* feedback);
 
     private:
         decoder_wrapper* m_wrapper;
