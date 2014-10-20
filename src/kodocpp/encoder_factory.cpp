b74c4838a426b32acc75c8d6eb5f2bd43d92e1b0
Jeppe Pihl <jpihl08@gmail.com>
2014-10-20 13:36:27 +0200

    template ftw

--- src/kodocpp/encoder_factory.cpp
+++ src/kodocpp/encoder_factory.cpp
@@ -8,6 +8,29 @@
 
 namespace kodo
 {
+    template<template <class, bool> KodoStack, class trace>
+    KodoStack pick_field(kodo::fieldtype field_type)
+    {
+        if(field_type == kodo::field_type.binary)
+        {
+            return new KodoStack<
+                fifi::binary,
+                trace>encoder_factory_wrapper();
+        }
+        else if(field_type == kodo::field_type.binary8)
+        {
+            return new KodoStack<
+                fifi::binary8,
+                trace>encoder_factory_wrapper();
+        }
+        else if(field_type == kodo::field_type.binary16)
+        {
+            return new KodoStack<
+                fifi::binary16,
+                trace>encoder_factory_wrapper();
+        }
+    }
+
     encoder_factory::encoder_factory(kodo::algorithm algorithm,
                                      kodo::fieldtype field_type,
                                      uint32_t max_symbols,
@@ -18,48 +41,13 @@ namespace kodo
         {
             if(!trace_enabled)
             {
-                if(field_type == kodo::field_type.binary)
-                {
-                    m_factory_wrapper =
-                        new kodo::full_rlnc_encoder<
-                            fifi::binary>encoder_factory_wrapper();
-                }
-                else if(field_type == kodo::field_type.binary8)
-                {
-                    m_factory_wrapper =
-                        new kodo::full_rlnc_encoder<
-                            fifi::binary8>encoder_factory_wrapper();
-                }
-                else if(field_type == kodo::field_type.binary16)
-                {
-                    m_factory_wrapper =
-                        new kodo::full_rlnc_encoder<
-                            fifi::binary16>encoder_factory_wrapper();
-                }
+                m_factory_wrapper = pick_field<
+                    full_rlnc_encoder, trace_disabled>(field_type);
             }
             else
             {
-                if(field_type == kodo::field_type.binary)
-                {
-                    m_factory_wrapper =
-                        new kodo::kodo::full_rlnc_encoder<
-                            fifi::binary, kodo::enable_trace>
-                        encoder_factory_wrapper();
-                }
-                else if(field_type == kodo::field_type.binary8)
-                {
-                    m_factory_wrapper =
-                        new kodo::full_rlnc_encoder<
-                            fifi::binary8, kodo::enable_trace>
-                        encoder_factory_wrapper();
-                }
-                    else if(field_type == kodo::field_type.binary16)
-                    {
-                        m_factory_wrapper =
-                            new kodo::full_rlnc_encoder<
-                                fifi::binary16, kodo::enable_trace>
-                            encoder_factory_wrapper();
-                    }
+                m_factory_wrapper = pick_field<
+                    full_rlnc_encoder, trace_enabled>(field_type);
             }
             assert(m_factory_wrapper);
         }
